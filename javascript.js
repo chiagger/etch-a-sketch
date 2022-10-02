2fd7329b080f82a9399766e07a1edefa5873ed01
chiagger <busca.chiara.cb@gmail.com>
2022-10-02 21:36:54 +0200

    add canvas size slider and functionality

--- javascript.js
+++ javascript.js
@@ -1,14 +1,19 @@
 const gridContainer = document.getElementById('drawhere');
+const DEFAULT_SIZE = 16;
 
 
 for (let i=0; i<16; i++) {
     for (let k=0; k<16; k++) {
         const square = document.createElement('div');
+        square.style.width= '50px';
+        square.style.height= '50px';
         gridContainer.appendChild(square);
     }
 }
 
+
 const anydiv = document.querySelectorAll("#drawhere div");
+const grid = document.getElementById('drawhere');
 
 let downCheck=false;
 document.body.onmousedown = () => (downCheck = true);
@@ -28,5 +33,52 @@ function changeColor(e) {
     
 }
 
+var slider = document.getElementById("myRange");
+var output = document.getElementById("sizeValue");
+slider.onmousemove = (e) => updateSizeValue(e.target.value);
+slider.onchange = (e) => changeSize(e.target.value);
+
+function changeSize(value) {
+    setCurrentSize(value)
+    updateSizeValue(value)
+    reloadGrid()
+  }
+
+  function updateSizeValue(value) {
+    output.innerHTML = `${value} x ${value}`;
+  }
+
+  function reloadGrid() {
+    clearGrid()
+    setupGrid(currentSize)
+  }
+
+  let currentSize = DEFAULT_SIZE;
+    function setCurrentSize(newSize) {
+    currentSize = newSize;
+  }
+
+  function clearGrid() {
+    grid.innerHTML = '';
+  }
 
 
+  
+  function setupGrid(size) {
+    let sizenr = 800/size;
+    let sizenrstring = sizenr.toString();
+    let sizeStringPx = sizenrstring.concat("", 'px');
+    for (let i=0; i< size*size; i++) {
+        const square = document.createElement('div');
+        square.style.width= sizeStringPx;
+        square.style.height= sizeStringPx;
+        gridContainer.appendChild(square);
+    }
+
+    const anydiv = document.querySelectorAll("#drawhere div");
+
+    anydiv.forEach(function(elem) {
+        elem.addEventListener('mouseover', changeColor);
+        elem.addEventListener('mousedown', changeColor);
+    });
+  }
